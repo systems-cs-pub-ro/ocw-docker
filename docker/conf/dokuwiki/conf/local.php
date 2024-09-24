9d4779d1bbe878c5b32da827a5d65952c77e2be5
Stancu Florin <niflostancu@gmail.com>
2024-09-24 16:47:52 +0300

    tpl: install & enable arctic template

--- docker/conf/dokuwiki/conf/local.php
+++ docker/conf/dokuwiki/conf/local.php
@@ -5,8 +5,16 @@
 
 $conf['title'] = 'OCW';
 $conf['license'] = 'cc-by-sa';
+$conf['template'] = 'arctic';
 $conf['useacl'] = 1;
 $conf['superuser'] = '@admin';
 $conf['disableactions'] = 'register';
 $conf['userewrite'] = 1;
 $conf['useslash'] = 1;
+
+/* Arctic template customizations */
+$conf['tpl']['arctic']['sidebar'] = 'right';
+$conf['tpl']['arctic']['main_sidebar_always'] = 0;
+$conf['tpl']['arctic']['left_sidebar_content'] = 'main,toc,user,group,namespace';
+$conf['tpl']['arctic']['right_sidebar_order'] = 'namespace,toc,user,group';
+$conf['tpl']['arctic']['right_sidebar_content'] = 'main,toc,user,group,namespace';
