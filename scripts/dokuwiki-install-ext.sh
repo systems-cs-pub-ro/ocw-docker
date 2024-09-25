4aea3597782576fe2b3a91ec63825606750c4161
Stancu Florin <niflostancu@gmail.com>
2024-09-25 11:24:09 +0300

    core: install farmer plugin

--- scripts/dokuwiki-install-ext.sh
+++ scripts/dokuwiki-install-ext.sh
@@ -2,6 +2,10 @@
 # Install dokuwiki extensions (plugins & templates)
 set -e
 
-git clone https://github.com/samfisch/dokuwiki-template-arctic.git \
+git clone "https://github.com/samfisch/dokuwiki-template-arctic.git" \
 	/var/www/html/lib/tpl.core/arctic
+
+# Install farmer plugin
+git clone "https://github.com/cosmocode/dokuwiki-plugin-farmer.git" \
+	/var/www/html/lib/plugins.core/farmer
 
